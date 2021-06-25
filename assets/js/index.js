3f2c5a6d196577d469353be30cb46d81e2dbd3bb
Mojaffar Ansari <mojaffaransari2016@gmail.com>
2021-06-25 20:49:41 +0530

    form validation removed

--- assets/js/index.js
+++ assets/js/index.js
@@ -209,64 +209,65 @@ themeButton.addEventListener("click", () => {
 
 
 // Client side validation
-function validateName() {
-  let name = document.getElementById('name').value;
-  if(name.length == 0) {
-    alert("Name can't be blank");
-    return false;
-  }
-  if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
-    alert("Please enter your correct name");//Validation Message
-    return false;
-  }
-  return true;
-}
+// function validateName() {
+//   let name = document.getElementById('name').value;
+//   if(name.length == 0) {
+//     alert("Name can't be blank");
+//     return false;
+//   }
+//   if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+//     alert("Please enter your correct name");//Validation Message
+//     return false;
+//   }
+//   return true;
+// }
 
 
-function validateTitle() {
-  let title = document.getElementById('title').value;
-  if(title.length == 0) {
-    alert("Title can't be blank");
-    return false;
-  }
-  if (!title.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
-    alert("Please enter a valid title");//Validation Message
-    return false;
-  }
-  return true;
-}
+// function validateTitle() {
+//   let title = document.getElementById('title').value;
+//   if(title.length == 0) {
+//     alert("Title can't be blank");
+//     return false;
+//   }
+//   if (!title.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+//     alert("Please enter a valid title");//Validation Message
+//     return false;
+//   }
+//   return true;
+// }
 
 
-function validateEmail() {
+// function validateEmail() {
 
-  var email = document.getElementById('email').value;
-  if(email.length == 0) {
-    alert("Email can't be blank") ;//Validation Message
-    return false;
+//   var email = document.getElementById('email').value;
+//   if(email.length == 0) {
+//     alert("Email can't be blank") ;//Validation Message
+//     return false;
 
-  }
+//   }
 
-  if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
-    alert("Please enter a correct email address") ;//Validation Message
-    return false;
+//   if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+//     alert("Please enter a correct email address") ;//Validation Message
+//     return false;
 
-  }
-  return true;
-}
+//   }
+//   return true;
+// }
 
-function validateForm() {
-  if (!validateName() || !validateEmail() || !validateTitle()) {
+// function validateForm() {
+//   if (!validateName() || !validateEmail() || !validateTitle()) {
 
-    alert("Form not submitted");//Validation Message
-    return false;
-  }
-  else {
-    submitted=true;
-    return true;
-  }
-}
+//     alert("Form not submitted");//Validation Message
+//     return false;
+//   }
+//   else {
+//     submitted=true;
+//     return true;
+//   }
+// }
 
 function resetForm(){
   let form = document.getElementById('myForm')
   form.reset();
+  return true;
 }
